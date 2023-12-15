d1e951b92dd46a304a49297bb0521957ff602860
Alexander Adimas <alexanderadimas@gmail.com>
2023-12-15 18:37:02 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -105,10 +105,10 @@
         <footer class="main-footer">
             {{-- To the right --}}
             <div class="float-right d-none d-sm-block">
-                <b>Version</b> 1.8.3
+                <b>Version</b> 1.9.1
             </div>
             {{-- Default to the left --}}
-            <strong>Copyright &copy; 2023 <a href="https://niagahoster.co.id">Niagahoster</a>.</strong> All rights
+            <strong>Copyright &copy; 2023 <a href="https://niagahoster.co.id">Domainesia</a>.</strong> All rights
             reserved.
         </footer>
     </div>
